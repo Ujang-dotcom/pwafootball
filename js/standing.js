32574869b802c6c891207464038d901be070dfdd
dhimas.ucarn@gmail.com <dhimas.ucarn@gmail.com>
2020-07-21 23:19:26 +0700

    fix br

--- js/standing.js
+++ js/standing.js
@@ -9,7 +9,7 @@ function standings(data) {
                     <td>
                     <a href="./pages/detail-pages/detailtim.html?id=${club.team.id}">
                     <p class="hide-on-small-only">
-                    <img class = "show-on-medium-and-up show-on-medium-and-down" src=${club.team.crestUrl} onerror="this.onerror=null;this.src='${club.team.crestUrl}';"  alt="logo club" style="float:left;width:22px;height:22px;margin-right:20px">
+                    <img class = "show-on-medium-and-up show-on-medium-and-down" src=${club.team.crestUrl} onerror="this.onerror=null;this.src='${club.team.crestUrl}';"  alt="logo club" style="float:left;width:22px;height:22px;margin-right:20px"><br>
                     ${club.team.name}
                     </p>
                     <p class="hide-on-med-and-up">
